f5cfa6c581da787447f55a36a6d76d28ef785398
Virin Tamprateep <v2tamprateep@gmail.com>
2019-01-11 18:25:00 -0800

    working board

--- GameState.py
+++ GameState.py
@@ -24,6 +24,7 @@ class GameState():
         self.boardState[self.ball] = BoardStates.Ball
 
     def applyAction(self, action: Action):
+
         assert type(action) is Action
 
         if action.type is ActionTypes.SetStone:
@@ -34,21 +35,32 @@ class GameState():
             assert False, "Invalid action type"
 
     def getAllPositions(self):
+
         return self.boardState.keys()
 
     def getBoardDelta(self):
+
         return self.boardDelta
 
     def getBallPosition(self):
+
         return self.ball
 
     def getStateAtPosition(self, position):
+
         return self.boardState[position]
 
     def getStonePositions(self):
+
         return [coordinate for coordinate in self.boardState if self.boardState[coordinate] is BoardStates.Stone]
 
+    def inBounds(self, coords):
+
+        r, c = coords[0], coords[1]
+        return (0 <= r <= self.rows-1) and (0 <= c <= self.cols-1)
+
     def moveBall(self, coordinates):
+
         self.validateMoveBall(coordinates)
         self.boardDelta = dict()
 
@@ -64,23 +76,24 @@ class GameState():
         self.boardDelta[coordinates[-1]] = BoardStates.Ball
 
     def setStone(self, coordinates):
+
         self.validateSetStone(coordinates)
         self.boardDelta = dict()
 
         self.boardState[coordinates[0]] = BoardStates.Stone
         self.boardDelta[coordinates[0]] = BoardStates.Stone
 
-    def inBounds(self, coords):
-        r, c = coords[0], coords[1]
-        return (0 <= r <= self.rows-1) and (0 <= c <= self.cols-1)
-
     def validateMoveBall(self, coordinates: list):
+
         assert len(coordinates) > 1, "MoveBall action requires at least 2 coordinate pairs"
-        for point in coordinates[:-1]:
+        assert self.boardState[coordinates[0]] == BoardStates.Ball, "MoveBall action must start with ball"
+
+        for point in coordinates[1:-1]:
             assert self.boardState[point] == BoardStates.Stone, "Ball must just over stones"
 
         assert self.boardState[coordinates[-1]] == BoardStates.Empty, "Ball must land in empty position"
 
     def validateSetStone(self, coordinate: list):
+
         assert len(coordinate) == 1, "SetStone action takes only one coordinate pair"
         assert (self.boardState[coordinate[0]] == BoardStates.Empty), "Stone can only be placed on an empty position"
--- phutballBoard.py
+++ phutballBoard.py
@@ -19,6 +19,7 @@ class Board(tk.Frame):
 
     def __init__(self, parent, gameState):
 
+        # board settings
         tk.Frame.__init__(self, parent)
         self.gameState = gameState
         self.rows = self.gameState.rows  # number of horizontal lines
@@ -31,13 +32,12 @@ class Board(tk.Frame):
         h = (self.rows - 1) * self.squareSize + 2 * self.vborder
 
         # Maintain references to pieces on the board to remove as necessary
-        self.boardObjects = list()
+        self.boardObjects = dict()
 
         self.board = tk.Canvas(parent, width=w, height=h)
         self.board.pack()
 
-        # self.clickable = False #can the player click on the screen?
-
+        # Initialize board
         for i in range(0, self.cols):
             x = self.hborder + i * self.squareSize
             self.board.create_line(x, self.vborder, x, self.vborder + self.squareSize * (self.rows - 1))
@@ -46,24 +46,92 @@ class Board(tk.Frame):
             y = self.vborder + i * self.squareSize
             self.board.create_line(self.hborder, y, self.hborder + self.squareSize * (self.cols - 1), y)
 
+        self.drawStone(self.gameState.getBallPosition(), football=True)
+
         self.board.bind("<Button-1>", self.onClick)
         self.board.bind("<Motion>", self.onMove)
 
-        # self.nextTurn() #start the game
+        # For gameplay
+        self.playerOneTurn = True
+        self.executingBallMoveAction = False
+        self.ballMovePositions = list()
 
     def clearBoard(self):
-        for obj in self.boardObjects():
-            self.board.delete(obj)
+
+        for coordinate in self.boardObjects.keys():
+            self.clearPosition(coordinate)
 
     def clearPosition(self, position):
+
         self.board.delete(self.boardObjects[position])
-        self.boardObjects.remove(position)
+        self.boardObjects[position] = None
+
+    def distance(self, position1, position2):
+
+        return abs(position1[0] - position2[0]) + abs(position1[1] - position2[1])
+
+    def drawStone(self, coords, football=False):
+        
+        r, c = coords
+        center = (self.hborder + c * self.squareSize, self.vborder + r * self.squareSize)
+        color = "black" if not football else "white"
+        rad = self.stonerad
+        boardObject = self.board.create_oval(
+            center[0]-rad,
+            center[1]-rad,
+            center[0]+rad,
+            center[1]+rad,
+            fill=color,
+            width=1,
+            outline="black")
+
+        self.boardObjects[coords] = boardObject
+
+    def executingBallMove(self, position, positionState):
+
+        if (positionState is BoardStates.Ball):
+            return
+
+        assert self.distance(self.ballMovePositions[-1], position) <= 2, "Stones must be adjacent during ball move."
+        self.ballMovePositions.append(position)
+
+        if (positionState is BoardStates.Empty):
+            action = Action(ActionTypes.MoveBall, self.ballMovePositions)
+            self.gameState.applyAction(action)
+            self.updateBoard()
+            self.ballMovePositions = list()
+            self.executingBallMoveAction = False
+
+    def onClick(self, event):
+
+        r, c = self.row_col(event.x, event.y)
+        positionState = self.gameState.getStateAtPosition((r, c))
+
+        if (self.executingBallMoveAction):
+            self.executingBallMove((r, c), positionState)
+            return
+
+        if (positionState is BoardStates.Empty):
+            # We just want to place a stone there
+            action = Action(ActionTypes.SetStone, [(r, c)])
+            self.gameState.applyAction(action)
+            self.updateBoard()
+
+        if (positionState is BoardStates.Ball):
+            # Initiate MoveBall action
+            self.ballMovePositions.append((r, c))
+            self.executingBallMoveAction = True
+
+    def onMove(self, event):
+        pass
 
     # get nearest grid point (in L_1 norm) to given coordinates
     def row_col(self, x, y):
 
         r = (y - self.vborder + 0.5 * self.squareSize) // self.squareSize
         c = (x - self.hborder + 0.5 * self.squareSize) // self.squareSize
+
+        assert self.gameState.inBounds((r, c)), "Selected point is not on the board."
         return (r, c)
 
     # use the game state to update the board GUI
@@ -79,46 +147,6 @@ class Board(tk.Frame):
                 self.drawStone(position, True)
             else:
                 assert False, "Found invalid BoardState when updating board"
-
-    #called on a click
-    def onClick(self,event):
-
-        # if not self.clickable: return
-        while (True):
-            r, c = self.row_col(event.x, event.y)
-            if (self.gameState.getStateAtPosition((r, c)) != BoardStates.Stone):
-                break
-
-        action = None
-        if (self.gameState.getStateAtPosition((r, c)) == BoardStates.Empty):
-            action = Action(ActionTypes.SetStone, [(r, c)])
-        else:
-            # The Ball was clicked. Now we need to select a sequence of stones until we hit an empty position
-            action = Action(ActionTypes.MoveBall, [(r, c)])
-            while (self.gameState.getStateAtPosition((r, c)) != BoardStates.Empty):
-                r, c = self.row_col(event.x, event.y)
-
-                if (self.gameState.getStateAtPosition((r, c)) == BoardStates.Stone):
-                    action.appendCoordinate((r, c))
-
-        self.gameState.applyAction(action)
-        self.updateBoard()
-        
-    #create stone, return ID
-    def drawStone(self, coords, football=False):
-        r,c = coords
-        center = (self.hborder + c * self.squareSize, self.vborder + r * self.squareSize)
-        color = "black" if not football else "white"
-        rad = self.stonerad
-        return self.board.create_oval(center[0]-rad, center[1]-rad,center[0]+rad,center[1]+rad, fill=color, width = 1, outline = "black")
-
-    #wait for a move
-    def acceptMove(self):
-        pass
-        #self.clickable = true
-
-    def onMove(self, event):
-        pass
 
 
 top = tk.Tk()
